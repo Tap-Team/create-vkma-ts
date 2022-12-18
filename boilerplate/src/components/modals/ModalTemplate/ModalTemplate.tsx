e7dcb0b5f94e628844927757a9534bb720e54006
Local <thetypgame@gmail.com>
2022-12-18 22:45:29 +0400

    change generic type

--- boilerplate/src/components/modals/ModalTemplate/ModalTemplate.tsx
+++ boilerplate/src/components/modals/ModalTemplate/ModalTemplate.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Div, ModalPage } from "@vkontakte/vkui";
 
-const ModalTemplate: React.FC<HTMLDivElement> = ({id}: HTMLDivElement): JSX.Element => {
+const ModalTemplate: React.FC<{id: string}> = ({id}): JSX.Element => {
   return (
     <ModalPage id={id}>
       <Div>
